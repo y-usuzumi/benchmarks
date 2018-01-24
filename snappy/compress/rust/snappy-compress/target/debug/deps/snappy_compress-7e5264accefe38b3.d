5f6acf4b47b06f527362febffafdb0855f7b09d5
Yukio Usuzumi <anohigisavay@gmail.com>
2018-01-25 00:54:34 +0800

    No target folder in rust should be version controled

--- snappy/compress/rust/snappy-compress/target/debug/deps/snappy_compress-7e5264accefe38b3.d
+++
@@ -1,5 +0,0 @@
-/home/kj/Lab/personal/benchmarks/snappy/compress/rust/snappy-compress/target/debug/deps/snappy_compress-7e5264accefe38b3: main.rs
-
-/home/kj/Lab/personal/benchmarks/snappy/compress/rust/snappy-compress/target/debug/deps/snappy_compress-7e5264accefe38b3.d: main.rs
-
-main.rs:
--- snappy/compress/rust/snappy-compress/target/debug/snappy-compress.d
+++
@@ -1,1 +0,0 @@
-/lab/Lab/personal/benchmarks/snappy/compress/rust/snappy-compress/target/debug/snappy-compress: /lab/Lab/personal/benchmarks/snappy/compress/rust/snappy-compress/src/main.rs
